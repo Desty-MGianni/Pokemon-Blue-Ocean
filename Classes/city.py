dd4184e429cb7adeed9552d3f5b398d0ad4fe0d5
Desty-MGianni <muccigianni23@gmail.com>
2025-07-20 19:06:25 +0200

    Sixth commit on main
    City class 40% done + pok_id table rearranged

--- Classes/city.py
+++ Classes/city.py
@@ -1,9 +1,55 @@
+from time import sleep
 from Classes.arena import Arena
 from Classes.shop import Shop
+from Classes.trainer import Player
+
 class City:
-    def __init__(self, name: str, has_arena: bool, arena_info: list):
+    def __init__(self, name: str, has_arena: bool, shop_rank: str, arena_info: dict = None):
         self.name = name
         if has_arena:
-            self.arena = Arena(arena_info[0],arena_info[1],arena_info[2],arena_info[3])
+            self.arena = Arena(arena_info['Name'],arena_info['Liste Dresseurs'],arena_info['Champion'],arena_info['Nom Badge'])
+        self.shop = Shop(rank_shop= shop_rank, city_name= self.name)
         
-    pass
+    def __pokemon_center(self, player: Player):
+        while True:
+            print(f"You are in {self.name}'s Pokémon center!")
+            try:
+                choice = int(input("What do you want to do ? \n1| Go to the nurse. \n2| Go to the PC. \n3| Exit"))
+            except ValueError:
+                choice = -1
+            match choice:
+                case 1:
+                    heal_input = input("Hello! Do you want me to heal your Pokémons ? Y/n").lower()
+                    match heal_input:
+                        case 'n' | 'no':
+                            break
+                        case _:
+                            sleep(0.5)
+                            print('.')
+                            sleep(0.5)
+                            print('.')
+                            sleep(0.5)
+                            print('.')
+                            sleep(0.5)
+                            for pokémon in player.list_pokémon:
+                                pokémon.health = pokémon.max_health
+                            print('Your Pokémons have been treated, they are in great shape!')
+                case 2:
+                    for pokémon in player.inventory.list_pokemon_pc:
+                        print(pokémon)
+                case 3:
+                    print("Thank you for you visit!")
+                    sleep(1)
+                    break
+                case _:
+                    print("Invalid input!")
+                    sleep(0.5)
+                    continue
+    def roaming(self, player: Player):
+        while True:
+            print(self.name)
+            player_choice = input("Where do you want to go? \n" \
+            "\t1| Pokémon Center \n" \
+            "\t2| Pokémon Shop \n" \
+            "\t3| Pokémon Gym\n")
+            ""
--- script.py
+++ script.py
@@ -1,8 +1,12 @@
 from Classes import *
 from Classes.pokemon import Pokémon
-from Classes.shop import Shop
 from Classes.trainer import Trainer, Player
 from Classes.battle import battle
+from Classes.city import City
+
+
+
+
 
 player = Player("bouffion",list_pokémon = [Pokémon(pok_id= 2), Pokémon(pok_id= 4), Pokémon(pok_id= 7), Pokémon(pok_id= 25), Pokémon(pok_id= 25), Pokémon(pok_id= 151,level= 40)])
 print(player.is_player)
@@ -11,11 +15,29 @@ player.inventory.update_inventory(item= 'Poké Ball',quantity= 5)
 player.inventory.update_inventory(item= 'Potion',quantity= 5)
 player.inventory.update_inventory(item= 'Hyper Potion',quantity= 2)
 player.inventory.update_inventory(item= 'Pierre Foudre',quantity= 1)
-player.inventory.add_badge_and_no_duplicate('Boulder Badge')
 #battle(player= player, opponent= trainer)
 #battle(player= player, opponent= trainer)
-test_shop = Shop("Max","Argenta")
-test_shop.shop(player)
+argenta = City(
+    name= 'Argenta',
+    has_arena= True, 
+    shop_rank= "Beginner",
+    arena_info= {
+        "Name": "Arène d'argenta", 
+        "Liste Dresseurs": [
+            Trainer(
+                name= 'Sous-champion 1', 
+                list_pokémon= [Pokémon(pok_id= 30)]), 
+            Trainer(
+                name= 'Sous-champion 2', 
+                list_pokémon= [Pokémon(pok_id= 53)])],
+        "Champion": Trainer(
+            name= 'Pierre',
+            list_pokémon= [
+                Pokémon(pok_id= 43,level= 13), 
+                Pokémon(pok_id= 67,level= 15)]),
+        "Nom Badge": 'Boulder Badge'},)
+
+argenta.roaming(player= player)
 player.use_item()
 pok1 = Pokémon(50,True,5)
 battle(player,pok1)
